6bc13c8b634a59601d701b5f197ca756b4866b93
Banoth Srikanth <83959475+srikanthbanoth@users.noreply.github.com>
2021-06-18 00:22:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,24 @@
 var http=require('http');
 var fs=require('fs');
 var url=require('url');
+const { callbackify } = require('util');
 http.createServer((req,res)=>{
    if(req.url=='/'){
-    //    callback('index.html')
-        filename='index.html'
-       fs.readFile(filename, (err, data) => {
-           if (err) {
-               res.writeHead(404, { 'Content-Type': 'text/html' });
-               res.write('404 Bad Request');
-           }
-           res.writeHead(200, { 'Content-Type': 'text/html' });
-           res.write(data)
-           res.end()
-       })
+       callback('index.html');
    }
-   if(req.url=='/contact.html'){
-       filename = 'contact.html'
-       fs.readFile(filename, (err, data) => {
-           if (err) {
-               res.writeHead(404, { 'Content-Type': 'text/html' });
-               res.write('404 Bad Request');
-           }
-           res.writeHead(200, { 'Content-Type': 'text/html' });
-           res.write(data)
-           res.end()
-       })
-   }
-    if (req.url == '/about.html') {
-        filename = 'about.html'
-        fs.readFile(filename, (err, data) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write('404 Bad Request');
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data)
-            res.end()
-        })
+    if (req.url == '/contact.html') {
+        callback('contact.html');
     }
+    if (req.url == '/about.html') {
+        callback('about.html');
+    }
+
+   function callback(filename){
+       fs.readFile(filename, (error, datafile) => {
+           if (error) throw error;
+           res.writeHead(200, { 'Content-Type': 'text/html' });
+           res.write(datafile);
+           return res.end();
+       })
+   }
 }).listen(8080)
